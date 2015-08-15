5b6171ace91ea53af13e180bde545e5d62f9be81
Melissa Steffan <msteffan2012@gmail.com>
2015-08-15 14:47:33 -0400

    made a unicorn fly

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// $("#unicorn").hide();
+$("input[type='submit']").on("click", function(){
+    // $("#unicorn").show();
+    $("#unicorn").addClass("animate")
+    update();
+});
+
+$("select").change(function() {
+    backToHorse();
+    $("#top-text").val("")
+    $("#bottom-text").val("")
+})
 
 
 function update() {
@@ -7,37 +19,11 @@ function update() {
         "bottom" : $("#bottom-text").val()
     });
     $("#meme-image").attr("src", link);
-    // $("#meme-link").text(link).attr("href", link);
+    $("#meme-image").attr("href", link);
 }
-// $("form").change(function() {
-//     update();
-// }).submit(function() {
-//     update();
-//     return false;
-// });
-$("input[type='submit']").on("click", function(){
-    update();
-});
 
 
-//
-//     $.ajax({
-//         url: "http://apimeme.com/meme?meme=Alright+Gentlemen+We+Need+A+New+Idea&top=Top+text&bottom=Bottom+text",
-//         //"http://apimeme.com/meme?meme="+image+"&top="+TopText+"&bottom="+BottomText, // The URL to the API.
-//         type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
-//         data: {}, // Additional parameters here
-//         dataType: 'json',
-//         success: function(data){
-//             $("#yourMeme").html(data)
-//         },
-//         error: function(err) {
-//             console.log(err);
-//         },
-//         beforeSend: function(xhr) {
-//         xhr.setRequestHeader("X-Mashape-Authorization", "chh9lOCWGsmsh8AQLQvuEH3d8DFAp1BtrIQjsnEsdviDvwe8un"); // Enter here your Mashape key
-//         }
-//     });
-//
-//
-//
-// })
+function backToHorse() {
+    $("#unicorn").removeClass("animate")
+
+}
